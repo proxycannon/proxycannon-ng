6df4236e493fa6c561ff054b056b81d99248ac9a
Snizz <traviskaun@gmail.com>
2018-10-26 17:20:35 -0600

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo -s
-cd /etc/openvpn/easy-rsa/
-source ./vars
-/etc/openvpn/easy-rsa/pkitool --initca 
-/etc/openvpn/easy-rsa/pkitool --server server
-/usr/bin/openssl dhparam -out /etc/openvpn/easy-rsa/keys/dh2048.pem 2048
-openvpn --genkey --secret /etc/openvpn/easy-rsa/keys/ta.key
-systemctl start openvpn@node-server.service
-systemctl start openvpn@client-server.service
-sysctl -w net.ipv4.ip_forward=1
+sudo cd /etc/openvpn/easy-rsa/
+sudo source ./vars
+sudo /etc/openvpn/easy-rsa/pkitool --initca 
+sudo /etc/openvpn/easy-rsa/pkitool --server server
+sudo /usr/bin/openssl dhparam -out /etc/openvpn/easy-rsa/keys/dh2048.pem 2048
+sudo openvpn --genkey --secret /etc/openvpn/easy-rsa/keys/ta.key
+sudo /etc/openvpn/easy-rsa/pkitool client01
+sudo /etc/openvpn/easy-rsa/pkitool node01
+sudo systemctl start openvpn@node-server.service
+sudo systemctl start openvpn@client-server.service
+sudo sysctl -w net.ipv4.ip_forward=1
