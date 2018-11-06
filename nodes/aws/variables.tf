8c9359ab903438db49451e529c490f49d81d00af
UrfinJusse <UrfinJusse@users.noreply.github.com>
2018-11-06 10:45:48 -0500

    Added missing "subnet_id" variable

--- nodes/aws/variables.tf
+++ nodes/aws/variables.tf
@@ -6,3 +6,10 @@ variable "aws_priv_key" {
 variable "count" {
   default = 2
 }
+
+# launch all exit nodes in the same subnet id
+# this should be the same subnet id that your control server is in
+# you can get this value from the AWS console when viewing the details of the control-server instance
+variable "subnet_id" {
+  default = "subnet-XXXXXXXX"
+}
