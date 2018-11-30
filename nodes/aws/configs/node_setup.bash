1da93962b735829488f3b97173f9a0edf9badf4a
jaredhaight <jared@ibm.com>
2018-11-30 13:21:06 -0800

    fixing the dumb typo here too

--- nodes/aws/configs/node_setup.bash
+++ nodes/aws/configs/node_setup.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo sysctl -w net.ipv4.ip_forward=1
-DEFAULTETH=`ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//"``
+DEFAULTETH=`ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//"`
 sudo iptables -t nat -A POSTROUTING -o $DEFAULTETH -j MASQUERADE
