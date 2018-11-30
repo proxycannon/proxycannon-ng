37a504f66f73739bab9f03afeb0bbe63d96449cd
jaredhaight <jared@ibm.com>
2018-11-30 11:04:00 -0800

    moved size and ami values to variables.tf

--- nodes/aws/variables.tf
+++ nodes/aws/variables.tf
@@ -7,6 +7,13 @@ variable "count" {
   default = 2
 }
 
+variable "ami" {
+  default = "ami-0f65671a86f061fcd"
+}
+variable "size" {
+  default = "t2.micro"
+}
+
 # launch all exit nodes in the same subnet id
 # this should be the same subnet id that your control server is in
 # you can get this value from the AWS console when viewing the details of the control-server instance
